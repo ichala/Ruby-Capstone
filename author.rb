2f1b739f0d6023b614f0aea0786e6c6ae457e3e0
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-01 16:26:32 +0300

    Add association between item and author

--- author.rb
+++ author.rb
@@ -1,2 +1,12 @@
 class author
+    attr_reader :items
+    def initialize
+        @items=[]
+    end
+
+    def add_item(item)
+        @items.push(item)
+        item.add_author(self)
+    end
+
 end
