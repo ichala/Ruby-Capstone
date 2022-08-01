6f2666690aadec30a387960fe5d826a5bea71818
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-01 16:37:14 +0300

    Add method to game class

--- game.rb
+++ game.rb
@@ -1,7 +1,11 @@
-class Game
+require_relative 'item'
+class Game < Item
     def initialize(last_played)
         @multiplayer
         @last_played_at = last_played
     end
 
+    def can_be_archived?
+        (Date.today.year - @last_played_at.year) < 2
+    end
 end
