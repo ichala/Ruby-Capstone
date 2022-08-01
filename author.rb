48c4ba4d13c6e621239838d1a3bea8b9aa73a0e5
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-01 20:31:53 +0300

    Add values to id

--- author.rb
+++ author.rb
@@ -1,8 +1,8 @@
 class Author
   attr_reader :items
 
-  def initialize(id, first_name, last_name)
-    @id = id
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..100)
     @first_name = first_name
     @last_name = last_name
     @items = []
