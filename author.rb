d33fc8338c557daa4e7622cf6aad8ade09ef19d1
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-01 20:19:38 +0300

    Add properties to class author

--- author.rb
+++ author.rb
@@ -1,7 +1,10 @@
 class Author
   attr_reader :items
 
-  def initialize
+  def initialize(id,first_name,last_name)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
     @items = []
   end
 
