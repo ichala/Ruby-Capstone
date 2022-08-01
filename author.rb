a15b6743f0f8519258eadf314dc341793e09832d
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-01 20:20:00 +0300

    Fix linters

--- author.rb
+++ author.rb
@@ -1,7 +1,7 @@
 class Author
   attr_reader :items
 
-  def initialize(id,first_name,last_name)
+  def initialize(id, first_name, last_name)
     @id = id
     @first_name = first_name
     @last_name = last_name
