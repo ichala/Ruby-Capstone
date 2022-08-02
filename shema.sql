fcf66f84e53b57cd5aa7636e1adb265957933157
mandela.t <manderat206@gmail.com>
2022-08-02 20:10:17 +0200

    Add schema for music and genre

--- shema.sql
+++ shema.sql
@@ -30,3 +30,17 @@ CREATE TABLE books (
 );
 
 
+CREATE TABLE genre (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE music_album (
+    id  INT,
+    name VARCHAR(100),
+    on_spotify BOOLEAN,
+    FOREIGN KEY(id) REFERENCES item(id)
+);
+
+
