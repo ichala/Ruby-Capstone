dbe195c3892c2c357c99e8affac39d653d602a87
ichala <alijandhd@gmail.com>
2022-08-02 14:08:52 +0100

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,24 @@
-CREATE database catalog;
+CREATE DATABASE catalog;
 
+CREATE TABLE item (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    genre_id INT,
+    author_id INT,
+    label_id INT,
+    publish_date DATE,
+    archived BOOLEAN,
+    PRIMARY KEY(id),
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id)
+);
+
+CREATE TABLE labels (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(100),
+    color VARCHAR(100),
+    PRIMARY KEY(id)
+);
 CREATE table games (
   id serial primary key,
   published_date date,
@@ -14,3 +33,13 @@ CREATE table authors (
     items int
     CONSTRAINT fk_items FOREIGN KEY (items) REFERENCES items (id)
 );
+
+CREATE TABLE books (
+    id  INT,
+    title VARCHAR(100),
+    publisher VARCHAR(100),
+    cover_state VARCHAR(100),
+    FOREIGN KEY(id) REFERENCES item(id)
+);
+
+
