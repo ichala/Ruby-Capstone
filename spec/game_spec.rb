bef63e1909a748824b1255e1f117da655c34a4a3
kwambiee <kwambokaj2.jk@gmail.com>
2022-08-02 15:06:01 +0300

    Fix linters

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,18 +2,18 @@ require_relative '../classes/game'
 require 'date'
 
 describe Game do
-    it 'should have a multiplayer value' do
-        game=Game.new('Mary', '12/12/2012')
-        expect(game.multiplayer).to eq('Mary')
-    end
+  it 'should have a multiplayer value' do
+    game = Game.new('Mary', '12/12/2012')
+    expect(game.multiplayer).to eq('Mary')
+  end
 
-    it 'should have a last_played date' do
-        game=Game.new('Mary', '12/12/2012')
-        expect(game.last_played_at).to eq('12/12/2012')
-    end
+  it 'should have a last_played date' do
+    game = Game.new('Mary', '12/12/2012')
+    expect(game.last_played_at).to eq('12/12/2012')
+  end
 
-    it 'should be able to be archived' do
-        game=Game.new('Mary', Date.new(2012,12,12))
-        expect(game.can_be_archived?).to eq(true)
-    end
+  it 'should be able to be archived' do
+    game = Game.new('Mary', Date.new(2012, 12, 12))
+    expect(game.can_be_archived?).to eq(true)
+  end
 end
